5eb2429fb0fb6cdfa9810a2aa2dd96883526e060
Jhoan Cuartas <jcuartas@serempre.com>
2021-09-06 20:45:52 -0500

    add Store class

---
+++ src/store.ts
@@ -0,0 +1,19 @@
+export class Store {
+  public incomes: number;
+  private productLoader: ProductLoader;
+  public inventory: Product[];
+
+  constructor(productLoader: ProductLoader) {
+    this.incomes = 0;
+    this.productLoader = productLoader;
+    this.loadProducts();
+  }
+
+  public addIncomes(income: number): void {
+    this.incomes += income;
+  }
+
+  private loadProducts(): void {
+    this.inventory = this.productLoader.loadProducts();
+  }
+}
